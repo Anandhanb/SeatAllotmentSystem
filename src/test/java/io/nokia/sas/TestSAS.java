b36ded379d22b9d713f9f095ba981e3002d46a23
Anandhan Balasubramanian <clickanandh@gmail.com>
2020-02-16 03:41:04 +0530

    Add Unit test files

---
+++ src/test/java/io/nokia/sas/TestSAS.java
@@ -0,0 +1,14 @@
+package test.java.io.nokia.sas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSAS {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
